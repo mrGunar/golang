f968a55dbf26a11374fa6d4f85747f09573b6030
mrGunar <plotkin.andrei@gmail.com>
2022-05-09 21:52:35 +0300

    web app

--- app/main.go
+++ app/main.go
@@ -2,15 +2,33 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"net/http"
 )
 
+type User struct {
+	name       string
+	age        uint16
+	balance    int16
+	avg_grades float64
+}
+
 func home_page(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello!")
+	tmpl, _ := template.ParseFiles("app/index.html")
+	user := User{"Alex", 30, 5000, 10.0}
+	tmpl.Execute(w, user)
+}
+
+func about_page(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "About myself")
+}
+
+func handleRequest() {
+	http.HandleFunc("/", home_page)
+	http.HandleFunc("/about", about_page)
+	http.ListenAndServe(":8080", nil)
 }
 
 func main() {
-	http.HandleFunc("/", home_page)
-	http.ListenAndServe(":8080", nil)
-	fmt.Println("s")
+	handleRequest()
 }
