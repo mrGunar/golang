d93cd6c5768233e82ddbdde21344db349ddb07fc
mrGunar <plotkin.andrei@gmail.com>
2022-05-10 21:07:56 +0300

    structures

--- general/structures.go
+++ general/structures.go
@@ -2,7 +2,32 @@ package main
 
 import "fmt"
 
+func (u User) getAllInfo() {
+	fmt.Println(u)
+	for _, x := range u.hobbies {
+		fmt.Println(x)
+	}
+
+	switch {
+	case u.status == true:
+		fmt.Printf("Status: %s", u.last_name)
+	case u.status == false:
+		fmt.Printf("Status: %s", u.name)
+	}
+}
+
+type User struct {
+	name      string
+	last_name string
+	age       uint16
+	city      string
+	male      string
+	status    bool
+	hobbies   []string
+}
+
 func main() {
 
-	fmt.Println()
+	user := User{"Alex", "Smith", 20, "Brighton", "m", true, []string{"footbal", "cookies", "guitar"}}
+	user.getAllInfo()
 }
